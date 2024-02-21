34dc84d9eb02cd710d1c211df95275340f9ede61
Santosh Shinde <santoshshinde2012@gmail.com>
2024-02-21 21:27:28 +0530

    Initial popup screen

---
+++ src/components/Popup.tsx
@@ -0,0 +1,12 @@
+import './Popup.css';
+
+const Popup = () => {
+  return (
+    <div className="popup-container">
+      <h1 className="popup-title">Chrome Extension!</h1>
+      <p>This is a popup created using React.js, TypeScript, and Vite.</p>
+    </div>
+  );
+};
+
+export default Popup;
